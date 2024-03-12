bea2fac90bb3cf443f62edb43f16604d96eb42a8
Thanchida <thanchida.a@ku.th>
2024-03-12 16:14:21 +0700

    finish Problem1

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,30 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+import math
 
-# TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    def setUp(self):
+        self.circle = Circle(3)
+        self.check = ''
+
+    def test_two_circle_have_positive_radius(self):
+        self.assertEqual(self.circle.get_radius(), 3)
+        radius = self.circle.get_radius()
+        self.assertEqual(self.circle.get_area(), math.pi*radius*radius)
+
+        second_circle = Circle(4)
+        result = self.circle.add_area(second_circle)
+        self.assertEqual(result.get_radius(), 5)
+
+    def test_result_should_have_same_radius(self):
+        second_circle = Circle(0)
+        result = self.circle.add_area(second_circle)
+        self.assertEqual(result.get_radius(), self.circle.get_radius())
+
+    def test_radius_is_not_negative(self):
+        with self.assertRaises(ValueError):
+            self.check = Circle(-5)
+
