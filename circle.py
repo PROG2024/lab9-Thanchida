fe0c500e92687346fbcac4bd287874bf8dbc1a81
Thanchida <thanchida.a@ku.th>
2024-03-12 22:07:06 +0700

    finish Problem2

--- circle.py
+++ circle.py
@@ -10,6 +10,11 @@ class Circle:
         
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        >>> negative_circle = Circle(-10)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -20,6 +25,11 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+        >>> first_circle = Circle(3)
+        >>> second_circle = Circle(4)
+        >>> new_circle = first_circle.add_area(second_circle)
+        >>> new_circle.get_radius()
+        5.0
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
@@ -38,38 +48,6 @@ class Circle:
 
     def __str__(self) -> str:
         return f"Circle({self.radius})"
-
-    def add(self, circle: Circle):
-        """
-        :param circle with radius = 4
-        :return: Circle withradius = 5
-
-        Example:
-        >>> first_circle = Circle(3)
-        >>> second_circle = Circle(4)
-        >>> first_circle.add(second_circle)
-        5.0
-        """
-        new_circle = self.add_area(circle)
-        new_radius = new_circle.get_radius()
-        return new_radius
-
-    def circle_radius(self, circle: Circle):
-        """
-        :param circle with negative radius
-        :raise: Exception
-
-        Example:
-        >>> normal_circle = Circle(8)
-        >>> negative_circle = Circle(-9)
-        >>> normal_circle.circle_radius(negative_circle)
-        Traceback (most recent call last):
-        ...
-        """
-        new_circle = self.add_area(circle)
-        new_radius = new_circle.get_radius()
-        if new_radius < 0:
-            raise ...
 
     __repr__ = __str__
 
